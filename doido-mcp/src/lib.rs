904658444ba8dd2aea152e1b82c236770bc58a66
Fabiano Martins <fabiano.paula.martins@gmail.com>
2026-06-26 19:56:27 -0300

    feat: removing useless doido package

--- doido-mcp/src/lib.rs
+++
@@ -1,8 +0,0 @@
-pub mod protocol;
-pub mod registry;
-pub mod server;
-
-pub use doido_mcp_macros::{mcp_resource, mcp_server, resource, tool};
-pub use protocol::{JsonRpcError, JsonRpcRequest, JsonRpcResponse};
-pub use registry::{ResourceDef, ResourceRegistry, ToolDef, ToolRegistry};
-pub use server::{mcp_router, McpState};
