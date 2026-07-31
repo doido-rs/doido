99a2bddea51bc4bcddab138e7afb637d2e245a58
Fabiano Martins <fabiano.paula.martins@gmail.com>
2026-07-31 14:14:08 -0300

    feat: fix the version flag result

--- doido-generators/tests/cli_test.rs
+++ doido-generators/tests/cli_test.rs
@@ -17,7 +17,7 @@ fn test_version_output() {
         .arg("--version")
         .assert()
         .success()
-        .stdout(predicate::str::contains("0.1.0"));
+        .stdout(predicate::str::contains(env!("CARGO_PKG_VERSION")));
 }
 
 #[test]
